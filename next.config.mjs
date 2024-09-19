2264fc98de39f18fc846c67751f7a169587dc930
Amr-ahmed-98 <67926050+Amr-ahmed-98@users.noreply.github.com>
2024-09-19 14:30:37 +0300

    updating file

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,19 @@
-/** @type {import('next').NextConfig} */
 import path from 'path';
-
-
-
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
-      },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 };
-
-module.exports = {
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  }
 
 export default nextConfig;
