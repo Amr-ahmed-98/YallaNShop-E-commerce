75dc25e7e8e92cf189c5cbbba68e5c10ab14944a
Amr-ahmed-98 <67926050+Amr-ahmed-98@users.noreply.github.com>
2024-09-19 14:02:18 +0300

    updating file

--- next.config.mjs
+++ next.config.mjs
@@ -17,4 +17,9 @@ const nextConfig = {
       },
 };
 
+module.exports = {
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  }
+
 export default nextConfig;
